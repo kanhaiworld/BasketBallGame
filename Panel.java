2c49130792da5e1f93c8ec7cc39b6c958f98c994
lab596 <86579136+lab596@users.noreply.github.com>
2022-05-10 20:20:16 -0700

    Update Panel.java

--- Panel.java
+++ Panel.java
@@ -2,10 +2,27 @@ import javax.swing.JPanel;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.Graphics;
+import java.awt.*;
+import javax.swing.*;
 
 
 public class Panel extends JPanel implements ActionListener
 {
+    public static void main(String[] args){
+        
+        Frame a = new Frame();
+        Dimension size= Toolkit.getDefaultToolkit().getScreenSize();
+        a.setSize((int) size.getWidth(),(int) size.getHeight());
+        
+        JLabel bg = new JLabel();
+        bg.setSize((int) size.getWidth(),(int) size.getHeight());
+        ImageIcon img = new ImageIcon("Red.jpg");
+        bg = new JLabel("",img,JLabel.CENTER);
+        bg.setBounds(0,0,(int) size.getWidth(),(int) size.getHeight());
+        bg.setBackground(Color.black);
+
+        a.add(bg);
+    }
     public Panel(){
         
     }
